f4db3d45e7e86bcfc51db38e5ae3c2a95227ac66
gialexan <alexromani.sc@gmail.com>
2023-01-27 22:07:48 +0100

    test for get quote in token word

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 14:21:55 by gialexan          #+#    #+#             */
-/*   Updated: 2023/01/26 22:21:13 by gialexan         ###   ########.fr       */
+/*   Updated: 2023/01/27 22:03:30 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ char	advance(t_scanner *scanner)
 	return (scanner->cmd[scanner->curr - 1]);
 }
 
-int	match(t_scanner *scanner, char expected)
-{
-	if (scanner->cmd[scanner->curr] != expected)
-		return (FALSE);
-	advance(scanner);
-	return (TRUE);
-}
+// int	match(t_scanner *scanner, char expected)
+// {
+// 	if (scanner->cmd[scanner->curr] != expected)
+// 		return (FALSE);
+// 	advance(scanner);
+// 	return (TRUE);
+// }
 
 void	init_scanner(t_scanner *scanner, const char *command)
 {
@@ -33,50 +33,85 @@ void	init_scanner(t_scanner *scanner, const char *command)
 	scanner->cmd = command;
 }
 
-t_token		*make_token(t_scanner *scanner, t_tk_type type)
+// t_token		*make_token(t_scanner *scanner, t_tk_type type)
+// {
+// 	t_token *token;
+
+// 	token = malloc(sizeof(t_token));
+// 	if (!token)
+// 		return (NULL);
+// 	token->lexema = ft_substr(scanner->cmd, scanner->start,
+// 			scanner->curr - scanner->start);
+// 	token->tk_type = type;
+// 	return (token);
+// }
+
+// t_token	*scan_token(t_scanner *scanner)
+// {
+// 	char	c;
+
+// 	c = advance(scanner);
+// 	if (c == '|')
+// 		return(make_token(scanner, TK_PIPE));
+// 	if (c == '<')
+// 	{
+// 		if (match(scanner, '<'))
+// 			return (make_token(scanner, TK_DLESS));
+// 		return (make_token(scanner, TK_LESS));		
+// 	}
+// 	if (c == '>')
+// 	{
+// 		if (match(scanner, '>'))
+// 			return (make_token(scanner, TK_DGREAT));
+// 		return (make_token(scanner, TK_GREAT));
+// 	}
+// 	else
+// 	{
+		
+// 	}
+// }
+
+int	is_quote(char c)
 {
-	t_token *token;
-
-	token = malloc(sizeof(t_token));
-	if (!token)
-		return (NULL);
-	token->lexema = ft_substr(scanner->cmd, scanner->start, scanner->curr - scanner->start);
-	token->tk_type = type;
-	return (token);
-}
-
-t_token	*scan_token(t_scanner *scanner)
-{
-	char	c;
-
-	c = advance(scanner);
-	if (c == '|')
-		return(make_token(scanner, TK_PIPE));
-	if (c == '<')
-	{
-		if (match(scanner, '<'))
-			return (make_token(scanner, TK_DLESS));
-		return (make_token(scanner, TK_LESS));		
-	}
-	if (c == '>')
-	{
-		if (match(scanner, '>'))
-			return (make_token(scanner, TK_DGREAT));
-		return (make_token(scanner, TK_GREAT));
-	}
-	return (0);
+	return (c == '\'' || c == '"');
 }
 
 int	main(void)
 {
 	t_scanner scanner;
-	const char *command = "<<<<<<<<<>>>>>|||||";
+	const char *command = "'ola'";  //'"palavra"'
 
 	init_scanner(&scanner, command);
-	while (command[scanner.curr] != '\0') 
+
+	char c = advance(&scanner);
+
+	printf("-> %d\n", is_quote(c));
+
+	// while (scanner.cmd[scanner.curr] != '\0')
+	// {
+	// 	char c = advance(&scanner);
+	// 	printf("-> %c\n", c);
+	// }
+	// printf("last: %d", scanner.cmd[scanner.curr]);
+
+	char close = c;
+	if (is_quote(c))
 	{
-		scanner.start = scanner.curr;
-		t_token *tk = scan_token(&scanner);
-		printf("%d | %s\n", tk->tk_type, tk->lexema);
+		c = advance(&scanner);
+		while (scanner.cmd[scanner.curr] && c != close)
+			c = advance(&scanner);
 	}
+	char *test = ft_substr(scanner.cmd, scanner.start, scanner.curr - scanner.start);
+	printf("%s\n", test);
+	printf("%d\n", scanner.curr);
 }
+
+
+
+
+	// while (command[scanner.curr] != '\0') 
+	// {
+	// 	scanner.start = scanner.curr;
+	// 	t_token *tk = scan_token(&scanner);
+	// 	printf("%d | %s\n", tk->tk_type, tk->lexema);
+	// }
